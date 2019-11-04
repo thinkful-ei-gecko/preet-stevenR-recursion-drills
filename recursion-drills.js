6d4d4b74628c9097872d2124b4525285700cab2d
preet-singh <preetsingh13@gmail.com>
2019-11-04 11:43:03 -0600

    Q4 complete

--- recursion-drills.js
+++ recursion-drills.js
@@ -10,6 +10,7 @@ const countSheep = function(number){
     return countSheep(number - 1);
 }
 countSheep(3);
+countSheep(55);
 
 //Q2: Power calculator
 //input = two nums and output some power num, ex: inpput (2, 3), output 8
@@ -35,4 +36,25 @@ const reverseString = function(str){
     }
     return reverseString(str.substr(1))+str.charAt(0);
 }
-console.log(reverseString("Jim"));
+console.log(reverseString("Jimmy boy"));
+console.log(reverseString("noshgniS"));
+
+
+//Q4 nth Triangular number
+const triangleNumber = function(number){
+   //base case
+   if(!number){
+       return 0;
+   } 
+   if(number === 1){
+       return 1
+   }
+   //recursive function
+   return number + triangleNumber(number - 1);
+}
+console.log(triangleNumber(3));//expect output to be 5
+console.log(triangleNumber(10));//expect output to be 55
+console.log(triangleNumber(34));//expect output to be ?
+
+//Q5 String spiltter
+
